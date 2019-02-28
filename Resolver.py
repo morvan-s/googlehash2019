0c551e8c49633253ee070f4c82745878a3fa2e26
Johan Sorette <sorette.johan@gmail.com>
2019-02-28 20:33:25 +0100

    add main + finish read function

--- Resolver.py
+++ Resolver.py
@@ -1,3 +1,6 @@
+from Photo import Photo
+from Slide import Slide
+
 class Resolver:
     def __init__(self, inputFile):
         self.inputFile = inputFile
@@ -5,16 +8,24 @@ class Resolver:
         self.verticalPhotos = []
         self.readInput(inputFile)
 
-    def readInput(self,file):
-        with open('input/'+file) as fp:
+    def readInput(self,inputFile):
+        with open(inputFile) as fp:
             numberimages = int(fp.readline())
-            for numero in range(0,numberimages):
+            for id in range(0,numberimages):
                 temp = fp.readline()[:-1].split(' ')
                 orientation = temp[0]
                 tags = []
                 for t in temp[2:]:
                     tags.append(t)
-
+                photo = Photo(id, orientation, tags) 
+                if orientation == 'H':
+                    slide = Slide([photo])
+                    for tag in tags:
+                        if tag not in self.slidesByTags.keys():
+                            self.slidesByTags[tag] = []
+                        self.slidesByTags[tag].append(slide)
+                else:
+                    self.verticalPhotos.append(photo)
 
     def mergeVerticalPhotosAsSlides():
         return
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from initialisation import *
-print('test')
+import sys
+from Resolver import Resolver
 
-read('a_example.txt',[],{})
+inputs = {
+    "a" : "a_example",
+    "b" : "b_lovely_landscapes",
+    "c" : "c_memorable_moments",
+    "d" : "d_pet_pictures",
+    "e" : "e_shiny_selfies"
+}
+
+if len(sys.argv) < 2:
+    print("Error : input name required !")
+    sys.exit()
+
+letter = sys.argv[1]
+
+if letter not in inputs.keys():
+    print("Error : wrong input name !")
+    sys.exit()
+
+Resolver("inputs/" + inputs[letter] + ".txt")
