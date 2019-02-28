11a7f3beead475ffc75a3b2ddc9b2254dcde3abf
Johan Sorette <sorette.johan@gmail.com>
2019-02-28 19:59:48 +0100

    Add model structure

---
+++ Resolver.py
@@ -0,0 +1,12 @@
+class Resolver:
+    def __init__(self, inputFile):
+        self.inputFile = inputFile
+        self.slidesByTags = {}
+        self.verticalPhotos = []
+        self.readInput(inputFile)
+    
+    def mergeVerticalPhotosAsSlides():
+        return
+
+    def solveSlideShow():
+        return
