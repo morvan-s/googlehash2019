a8a27f44f9f0a128217059ce37c936c1437310bc
Johan Sorette <sorette.johan@gmail.com>
2019-02-28 19:59:48 +0100

    Add model structure

---
+++ Photo.py
@@ -0,0 +1,5 @@
+class Photo:
+    def __init__(self, id, orientation, tags):
+        self.id = id
+        self.orientation = orientation
+        self.tags = tags
---
+++ Resolver.py
@@ -0,0 +1,11 @@
+class Resolver:
+    def __init__(self, inputFile):
+        self.inputFile = inputFile
+        self.slidesByTags = {}
+        self.verticalPhotos = []
+        self.readInput(inputFile)
+    
+    def mergeVerticalPhotosAsSlides():
+        return
+
+    def solve
